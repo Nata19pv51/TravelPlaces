1cb76bf1d2d63c8edc4dfa76c004950a446b4769
Natasha <nidialkova@ukr.net>
2018-02-09 23:55:18 +0200

    Change JDBCDisplayNoteDao

--- src/main/java/org/mycompany/myname/controller/LoadServlet.java
+++ src/main/java/org/mycompany/myname/controller/LoadServlet.java
@@ -2,6 +2,8 @@ package org.mycompany.myname.controller;
 
 import com.google.gson.Gson;
 import org.mycompany.myname.model.dao.implement.JDBCDisplayNote;
+import org.mycompany.myname.model.dao.implement.JDBCDisplayNoteDao;
+import org.mycompany.myname.model.entity.DisplayNote;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,9 +22,20 @@ public class LoadServlet extends HttpServlet {
 //
 //        Gson gson = new Gson();
 //        String result = gson.toJson(noteList);
+
+//        FindAll getAll = new FindAll();
+//        List<DisplayNote> notes =  getAll.getDisplayNote(1);
+//        Gson gson = new Gson();
+//        String jsonString = gson.toJson(notes);
+//        httpServletResponse.getWriter().print(jsonString);
+
         List<JDBCDisplayNote> nodes =  JDBCDisplayNote.getDisplayNotesByUserID(1);
         Gson gson = new Gson();
         String jsonString = gson.toJson(nodes);
         httpServletResponse.getWriter().print(jsonString);
+//        System.out.println(jsonString);
+
+
+        //System.out.println(jsonString);
     }
 }
