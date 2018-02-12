3e070b01b0754513b2617d057ad17682f33f6a55
Natasha <nidialkova@ukr.net>
2018-02-12 17:46:32 +0200

    Edit text of note

---
+++ src/main/java/org/mycompany/myname/controller/EditText.java
@@ -0,0 +1,4 @@
+package org.mycompany.myname.controller;
+
+public class EditText {
+}
--- src/main/webapp/resources/js/load.js
+++ src/main/webapp/resources/js/load.js
@@ -4,6 +4,7 @@ $("document").ready(function () {
 
 function loadData() {
     $('#homeInformation').remove();
+    $('#notes').prop("disabled", true);
     $.ajax("servletInNotes",
         {
             success: setNotesContent,
@@ -15,12 +16,84 @@ function loadData() {
 function setNotesContent(data, status, jqxhr) {
     data = JSON.parse(data)
     var info = $("#div")
+    info.append(
+        "<div class=\"text-center mb-5\" id=\"searcharea\">" +
+            "<div>" +
+                "<label for=\"search\">Live Search</label>" +
+                "<p>Enter the info about note</p>" +
+                "<input type=\"search\" name=\"search\" id=\"search\" placeholder=\"Info\"/>" +
+            "</div>" +
+        "</div>" +
+        "<div id=\"listNotes\">");
+
     data.forEach(function (item, i, data) {
-        info.append(
-            "<p> USER ID: " + item.userId + "</p>" +
-            "<p>Date of creation: " + item.time + "</p>" +
-            "<p>Coord : " + item.coordination + "</p>" +
-            "<p>Text : " + item.text + "</p>")
+        info.append("<div class=\"divNotes\">" +
+                        "<h4>" + item.time + "</h4>" +
+                        "<div class=\"textNote\">" + item.text + "</div>" +
+                     "</div>");
+        console.log("Fill date and text of note");
+//        $(".textNote").dblclick(function() {
+//           console.log("Hello");
+//           $('div#textNote').each(function() {
+//                $(this).replaceWith("<textarea id='textNote'>" + $(this).html() + "</textarea>")
+//           });
+//        });
     });
+    info.append("</div>")
     $("#insert_div").html(info);
+
+    $(".textNote").dblclick(function() {
+              console.log("Hello");
+             $(this).replaceWith("<textarea id='textNote'>" + $(this).html() + "</textarea>");
+        });
 }
+
+function edit(){
+    console.log("Hello");
+    $('div#textNote').each(function() {
+        $(this).replaceWith("<textarea id='textNote'>" + $(this).html() + "</textarea>");
+    });
+}
+//        info.append(
+////            "<li>" +
+//            "<div id=\"edit\">" +
+//                "<h4>" + item.time + "<h4>" +
+//                "<div id=\"textNote\">" + item.text + "</div>" +
+//            "</div>")});
+//            "<p> USER ID: " + item.userId + "</p>" +
+//            "<p>Date of creation: " + item.time + "</p>" +
+//            "<p>Coord : " + item.coordination + "</p>" +
+//            "<p>Text : " + item.text + "</p>")
+
+
+
+
+//function edit() {
+//    $.ajax("servletInNotes",
+//        {
+//            success: editText,
+//            type: "GET",
+//            dataType: "text"
+//        });
+//}
+//
+//function editText() {
+//    data = JSON.parse(data)
+//    data.forEach(function (item, i, data) {
+//        $('#textNote').replaceWith("<textarea id=\"textNote2\">" + item[i].text + "</textarea>");
+//    });
+//}
+
+
+
+
+// var note_div = $("div")
+//        note_div.html = "<h4>" + item.time + "<h4>" +
+//                        "<div id=\"textNote\">" + item.text + "</div>";
+//
+//        note_div.dblclick(function() {
+//                        console.log("Hello");
+//                //        $('div#textNote').each(function() {
+//                //          $(this).replaceWith("<textarea id='textNote'>" + $(this).html() + "</textarea>")
+//                //        });
+//                      });
