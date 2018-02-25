9e5d1ae86f4735d02040cb3ddb2e94ef058cb0ca
Natasha <nidialkova@ukr.net>
2018-02-25 22:09:32 +0200

    Show one note

--- src/main/java/org/mycompany/myname/controller/Notes/OpenOneNoteServlet.java
+++ src/main/java/org/mycompany/myname/controller/Notes/OpenOneNoteServlet.java
@@ -13,26 +13,12 @@ import java.io.PrintWriter;
 public class OpenOneNoteServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
-        String idNote =  httpServletRequest.getParameter("idNote");
-        int id = Integer.parseInt(idNote);
-        JDBCDisplayNote note =  JDBCDisplayNote.getNoteByID(id);
-
-        String html = "<jsp:include page=\"head.jsp\"/>" +
-                "<jsp:include page=\"nav.jsp\"/>";
-        html += "<div class=\"divNotes mb-2\">" +
-                "<input type=\"hidden\" class=\"idNote\" name=\"idNote\" value=\"" + note.getNoteId() + "\"/>" +
-                "<h4 class=\"col-sm-4 timeNote\">" + note.getTime() + "</h4>" +
-                "<div class=\"textNote\" name=\"textNote\">" + note.getText() + "</div>" +
-                "</div>";
-        html += "<jsp:include page=\"script.jsp\"/>" +
-                "<script src=\"resources/js/loadOneNote.js\"></script>" +
-                "</body>" +
-                "</html>";
-
-        PrintWriter printWriter = httpServletResponse.getWriter();
-        printWriter.print(html);
-        printWriter.close();
-//        RequestDispatcher rd=getServletContext().getRequestDispatcher("/WEB-INF/Notes/notes.jsp");
-//        rd.forward(httpServletRequest,httpServletResponse);
+       // httpServletResponse.setContentType("text/html");
+       // httpServletRequest.setAttribute("name", "value234dfg23345243");
+        try {
+            httpServletRequest.getRequestDispatcher("WEB-INF/Notes/oneNote.jsp").include(httpServletRequest, httpServletResponse);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        }
     }
 }
